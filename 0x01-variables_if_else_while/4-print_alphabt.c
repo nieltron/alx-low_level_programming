8d9e0218bd980db10c8d1f22cf7df37c782bc7d1
nieltron <dfiseha8@gmail.com>
2022-06-03 12:13:38 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * if condition
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lower;
+
+	for (lower = 'a' ; lower <= 'z' ; lower++)
+	{
+		if (lower != 'e' && lower != 'q')
+		{
+			putchar(lower);
+		}
+	}
+	putchar('\n');
+	return (0);
+	
+}
