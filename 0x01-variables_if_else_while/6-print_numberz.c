d67c201b816934e3ec38d1dda7d1912e4d6b1af0
nieltron <dfiseha8@gmail.com>
2022-06-03 12:40:37 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		putchar(a + '0');
+	}
+	putchar('\n');
+	return (0);
+}
