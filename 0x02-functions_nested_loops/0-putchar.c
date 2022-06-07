003e284c8c73114adb913cca0a055a0005984033
nieltron <dfiseha8@gmail.com>
2022-06-07 06:54:45 -0700

    1 alphabet c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * for loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char ch[] = "_putchar";
+	
+	for (i = 0 ; i <= 7 ; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
