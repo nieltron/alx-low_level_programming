8b098c7dca61e1d4f20c2f3621062a1ad5344280
nieltron <dfiseha8@gmail.com>
2022-06-13 09:30:41 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include main.h
+/**
+ * reset_to_98 - takes a pointer to a paremeter & updates its value
+ *
+ * @n: variable
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
