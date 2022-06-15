9f2ff9f64dfa98e07c189529840304c6d5d247f0
nieltron <dfiseha8@gmail.com>
2022-06-15 14:13:39 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,14 +6,21 @@
  * @n: amount of byte from src
  * Return: the poiner to dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++
-			dest[i] = src[i];
-	for ( ; i < n; i++)
-			dest[i] = '\0';
+	for (i = 0 ; src[i] != '\0' && i < n ; i++)
+	{
+		*(dest + i) = src[i];
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
