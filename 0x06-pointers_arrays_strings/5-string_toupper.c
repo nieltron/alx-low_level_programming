ef0f2e2ae1e17208ce5cecd57278ac5dd72f0c85
nieltron <dfiseha8@gmail.com>
2022-06-15 15:24:59 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *string_toupper - change lower to uppercase
+ * @str: input variable
+ * Return: pointer to dest
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] -32;
+		}
+	}
+	return (str);
+}
